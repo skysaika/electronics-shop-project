182f2cf4522386a78650491e9efb30a7a1293542
Saysuu <skysaika@yandex.ru>
2023-04-05 17:45:02 +0300

    добавила в Item super() для MixinKeyboardLanguage

--- src/item.py
+++ src/item.py
@@ -16,11 +16,12 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
+        super().__init__()
         self.__name = name
         self.price = price
         self.quantity = quantity
-
         Item.all.append(self)
+
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
